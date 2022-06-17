9e7e124268bc318e190def2cbbf4a2ab53bc3fc6
Atra Mors <blllsk@ya.ru>
2022-06-17 22:16:27 +0300

    -

---
+++ asd.py
@@ -0,0 +1,14 @@
+x = int(input())
+def check(x=x, counter=0):
+	for num in x:
+		ind = x.index(num)
+		ind = -ind
+		if num == x[ind - 1]:
+			counter += 1
+	if counter == len(x):
+		return True
+	else:
+		return False
+def smth(x):
+	x = ' '.join(str(x)).split()
+	print(check(x))
